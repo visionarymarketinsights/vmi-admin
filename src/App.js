494932c72796cbcdb4f2aa0357ff337a724193ae
sidpandagle <adi.pandagle@gmail.com>
2023-12-04 23:25:46 +0530

    Search Functionality added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AllReport from './pages/AllReport';
-import Offering from './pages/Offering';
-import IndustryReport from './pages/IndustryReport';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import ScrollToTop from './utils/ScrollToTop'
-import AllPressRelease from './pages/AllPressRelease';
-import PressRelease from './pages/PressRelease';
-import BuyNow from './pages/BuyNow';
-import AddReport from './pages/admin/Report/AddReport';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ReportList from './pages/admin/Report/ReportList';
+import AddReport from './pages/admin/Report/AddReport';
 import EditReport from './pages/admin/Report/EditReport';
-import Careers from './pages/Careers';
 import AddPressRelease from './pages/admin/PressRelease/AddPressRelease';
 import PressReleaseList from './pages/admin/PressRelease/PressReleaseList';
 import EditPressRelease from './pages/admin/PressRelease/EditPressRelease';
-import SearchList from './pages/SearchList';
 import { HelmetProvider } from 'react-helmet-async';
 import EditPrice from './pages/admin/Price/EditPrice';
 import PriceList from './pages/admin/Price/PriceList';
